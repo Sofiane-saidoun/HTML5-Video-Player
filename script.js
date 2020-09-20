84e3823c3588789ccc6e72f25c878aa4f47bd108
Sofiane <sofiane.saidoun@gmail.com>
2020-09-20 05:08:29 +0200

    commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const player = document.querySelector(".player");
+const video = document.querySelector(".viewer");
+const progress = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress__filled");
+const buttonPlayer = document.querySelector(".player__button");
+const ranges = document.querySelectorAll("[type='range']");
+const skipButtons = document.querySelectorAll("[data-skip]");
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? " | | " : " ► ";
+  buttonPlayer.textContent = icon;
+}
+
+function skip() {
+  video.currentTime = video.currentTime + parseInt(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+buttonPlayer.addEventListener("click", togglePlay);
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+let mouseDown = false;
+progress.addEventListener("mousemove", (e) => mouseDown && scrub(e));
+progress.addEventListener("mousedown", () => (mouseDown = true));
+progress.addEventListener("mouseup", () => (mouseDown = false));
+progress.addEventListener("click", scrub);
